f551f3c2244cb21dfc5fc7b8954a125d507286ff
Tommy Whitney <46704616+tommywhitney@users.noreply.github.com>
2024-02-01 17:14:21 -0500

    update telescope nav keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,10 +11,20 @@ return {
   {
     "nvim-telescope/telescope-ui-select.nvim",
     config = function()
+      local actions = require("telescope.actions")
+
       require("telescope").setup {
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown {
+            }
+          }
+        },
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
             }
           }
         }
