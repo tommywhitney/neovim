3a7deecd86c21441a045357ccc2ea3c4ef0bff92
Tommy Whitney <46704616+tommywhitney@users.noreply.github.com>
2024-02-24 16:38:11 -0500

    show dotfiles in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,14 @@ return {
 	},
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>")
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
 	end,
 }
---
+++ lua/plugins/nvimr.lua
@@ -0,0 +1,5 @@
+return {
+  "jalvesaq/Nvim-R",
+	config = function()
+	end,
+}
