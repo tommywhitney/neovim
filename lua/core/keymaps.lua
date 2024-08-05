e36b6a6caf56294c7843d019aef6ef8e8131c29e
Tommy Whitney <46704616+tommywhitney@users.noreply.github.com>
2024-08-05 10:12:36 -0400

    update dependencies

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -6,3 +6,6 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sk", "10<C-w>>", { desc = "Increase window width" })
+keymap.set("n", "<leader>sj", "10<C-w><", { desc = "Decrease window width" })
+
