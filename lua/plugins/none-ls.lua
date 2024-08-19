108bc0f625e12de53d27cec2f658dcdcadc272da
Tommy Whitney <46704616+tommywhitney@users.noreply.github.com>
2024-08-19 10:07:04 -0400

    remove black/isort

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,8 +7,6 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
 				null_ls.builtins.completion.spell,
 			},
 		})
